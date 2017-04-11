3509389ceabe9eb48e70f3b4fb52e306d969f700
vico <victor@smartcitizen.me>
2017-04-11 22:53:22 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -1,15 +1,22 @@
-cd esp
-platformio run
-cd ../sam
-#platformio run --target upload
-platformio run
-uploadBOSSA .pioenvs/zeroUSB/firmware.bin
-sleep 3
+# Build and upload SAM code
+cd sam
+platformio run --target upload
+
+# Big pause to let the kit boot even when it doesn't have sdcard (takes longer)
+sleep 5
+
+# Ask for flash mode (bridge Serial ports)
 echo "** Asking for ESP flash mode... **"
 echo "set mode esp flash" >> /dev/ttyACM0
 
-sleep 1
+# Build ESP code
+cd ../esp
+platformio run
 
-cd ..
-esptool.py --port /dev/ttyACM0 write_flash --flash_size=detect 0x00000 esp/.pioenvs/esp12e/firmware.bin
+# Upload ESP code
+# esptool.py --port /dev/ttyACM0 write_flash --flash_size=detect 0x00000 esp/.pioenvs/esp12e/firmware.bin
+/usr/bin/esptool -cp /dev/ttyACM0 -ca 0x00000 -cf esp/.pioenvs/esp12e/firmware.bin
+
+
 echo "** Remember you have to reset your kit after uploading firmwares!!! **"
+echo "Try clicking the button..."

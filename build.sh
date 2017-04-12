9030e6ab276bd894f9cace48113739f7f9d02a2e
vico <victor@smartcitizen.me>
2017-04-12 11:16:01 +0200

    Fix for last commit

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cd sam
 platformio run --target upload
 
 # Big pause to let the kit boot even when it doesn't have sdcard (takes longer)
-sleep 5
+sleep 3
 
 # Ask for flash mode (bridge Serial ports)
 echo "** Asking for ESP flash mode... **"
@@ -15,7 +15,7 @@ platformio run
 
 # Upload ESP code
 # esptool.py --port /dev/ttyACM0 write_flash --flash_size=detect 0x00000 esp/.pioenvs/esp12e/firmware.bin
-/usr/bin/esptool -cp /dev/ttyACM0 -ca 0x00000 -cf esp/.pioenvs/esp12e/firmware.bin
+/usr/bin/esptool -cp /dev/ttyACM0 -ca 0x00000 -cf .pioenvs/esp12e/firmware.bin
 
 
 echo "** Remember you have to reset your kit after uploading firmwares!!! **"

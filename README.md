59f3672b3fee38d9a50b57c43c2243473b61b76c
Viktor Smari <viktorsmari@gmail.com>
2017-09-18 12:32:34 +0200

    Simplify button CSS. Add mock-api + readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SmartCitizenKit
 Developer version of Smart Citizen Kit 1.5 Firmware
+
+
+
+### The Mock Api (optional development tool)
+
+Instead of going offline to talk to the kit via WIFI, there is a /mock-api folder where you can start your mock API.
+
+`cd mock-api`
+
+`npm install`
+
+`npm run start`
+
+And in */esp/data/index.html* change the apiUrl to 'localhost:3000' instead of '192.168.1.1'
---
+++ mock-api/server.js
@@ -0,0 +1,46 @@
+var express = require('express'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    port = process.env.PORT || 3000;
+var fs = require("fs");
+
+app.use(cors())
+
+// Tell express to use the body-parser middleware and to not parse extended bodies
+//app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+  res.send('This API mocks the SCK API, for development purposes. This file is at mock-api/servers.js');
+})
+
+app.get('/cors', function(req, res, next){
+  res.json({msg: 'This is CORS enabled for all origins!'})
+})
+
+app.get('/aplist', function(req, res){
+  fs.readFile(__dirname + "/" + "data.json", "utf8", function(err, data){
+    setTimeout( (function() {
+      res.end(data);
+    }), 900)
+  });
+})
+
+app.post('/connectwifi', function(req,res){
+  // Wait 1 sec to emulate Wifi connection latency
+  console.log(req.body);
+  setTimeout( (function() {
+      res.end('Connected to wifi!');
+  }), 1500)
+})
+
+
+// Middleware to catch wrong urls
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found!'})
+});
+
+app.listen(port);
+
+console.log('App started on port: ' + port);

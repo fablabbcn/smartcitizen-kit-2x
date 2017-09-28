9a05869705c56d9cb030a2a391216286747c3ebe
Viktor Smari <viktorsmari@gmail.com>
2017-09-28 16:25:51 +0200

    Update testing

--- mock-api/casperjs/test.js
+++ mock-api/casperjs/test.js
@@ -1,29 +1,26 @@
 var x = require('casper').selectXPath;
 
-casper.test.begin("Find something on our github webpage", 1, function(test) {
-    casper.start('https://fablabbcn.github.io/smartcitizen-kit-15/esp/data/', function() {
-    }).then(function() {
-        test.assertEvalEquals(function(){
-            return document.querySelectorAll('#wifisetup > form > .field-group')[1].outerText;
-        }, 'Password\n');
-    }).run(function() {
-        test.done();
-    });
+casper.test.begin("Find a few elements on our github webpage", 4, function(test) {
+  casper.start('https://fablabbcn.github.io/smartcitizen-kit-15/esp/data/', function() {
+  }).then(function() {
+    test.assertEvalEquals(function(){
+      return document.querySelectorAll('#wifisetup > form > .field-group')[1].outerText;
+    }, 'Password\n');
+    test.assertTitleMatch(/^SCK/i, 'The page title starts with SCK');
+    test.assertExists('#main', '#main exist');
+    test.assertExists('form', 'form exist');
+  }).run(function() {
+    test.done();
+  });
 });
 
-casper.test.begin("Fill in the form", 1, function(test) {
+casper.test.begin("Fill in the form and submit", 0, function(test) {
   casper.start('https://fablabbcn.github.io/smartcitizen-kit-15/esp/data/');
 
   casper.then(function(){
+    console.log(' ---- waiting 1 sec for page load (github) ---- ');
+    casper.wait(1400, function(){
 
-    console.log('waiting 2 sec');
-    casper.wait(2000, function(){
-      var x = this.echo(this.getTitle());
-
-      test.assertTitleMatch(/^SCK/i, 'The title starts with SCK');
-      test.assertExists('body', 'We have a body');
-      //test.assertExists('form', 'We have a form');
-      //test.assertExists('label', 'We have a label');
 
       //console.log("Page Title " + document.location.href)
       //console.log(document.querySelector('form'))
@@ -31,19 +28,17 @@ casper.test.begin("Fill in the form", 1, function(test) {
 
       //this.captureSelector('captures/schools-nav-open.png', '.school_nav.content-panel');
 
+      this.waitForSelector('form', function(){
+        this.fill('form', {
+          'token': '123456',
+          'password': 'SuperPassword'
+        }, true);
 
-      //this.waitForSelector('form', function(){
-      //  this.fill('form', {
-      //    'token': '123456',
-      //    'password': 'abcvd'
-      //  }, true);
-      //});
+        //casper.capture('xx.png');
+        casper.click('#connect');
+      });
 
-      casper.capture('xx.png');
 
-      //this.thenClick(x('//*[@id="app"]/form/div[4]/button[1]'), function(){
-      //  console.log("Connecting ...");
-      //});
 
     });
   }).run(function() {

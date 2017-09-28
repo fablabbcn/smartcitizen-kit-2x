bfa4d1cde6e65213758d00c2e04262ac52f28fd3
Viktor Smari <viktorsmari@gmail.com>
2017-09-28 15:42:16 +0200

    Add title to webpage, add tests

--- mock-api/casperjs/test.js
+++ mock-api/casperjs/test.js
@@ -1,22 +1,56 @@
-casper.test.begin("Find something on a webpage", 1, function(test) {
-    casper.start('https://fablabbcn.github.io/smartcitizen-kit-15/esp/data/', function() {
-        //this.waitForSelector('form[action="/search"]');
-    }).then(function() {
-        test.assertEvalEquals(function(){
-            return document.querySelector('h2').outerText;
-        }, 'Wifi Setup');
-    }).run(function() {
-        test.done();
-    });
+var x = require('casper').selectXPath;
 
+casper.test.begin("Find something on our github webpage", 1, function(test) {
     casper.start('https://fablabbcn.github.io/smartcitizen-kit-15/esp/data/', function() {
     }).then(function() {
         test.assertEvalEquals(function(){
-            return document.querySelector('body > #app > form > .field-group > label').outerText;
-        }, 'SSID');
+            return document.querySelectorAll('#wifisetup > form > .field-group')[1].outerText;
+        }, 'Password\n');
     }).run(function() {
         test.done();
     });
+});
+
+casper.test.begin("Fill in the form", 1, function(test) {
+  casper.start('https://fablabbcn.github.io/smartcitizen-kit-15/esp/data/');
+
+  casper.then(function(){
+
+    console.log('waiting 2 sec');
+    casper.wait(2000, function(){
+      var x = this.echo(this.getTitle());
+
+      test.assertTitleMatch(/^SCK/i, 'The title starts with SCK');
+      test.assertExists('body', 'We have a body');
+      //test.assertExists('form', 'We have a form');
+      //test.assertExists('label', 'We have a label');
+
+      //console.log("Page Title " + document.location.href)
+      //console.log(document.querySelector('form'))
+      //casper.waitUntilVisible('.school_nav.content-panel');
+
+      //this.captureSelector('captures/schools-nav-open.png', '.school_nav.content-panel');
+
+
+      //this.waitForSelector('form', function(){
+      //  this.fill('form', {
+      //    'token': '123456',
+      //    'password': 'abcvd'
+      //  }, true);
+      //});
+
+      casper.capture('xx.png');
+
+      //this.thenClick(x('//*[@id="app"]/form/div[4]/button[1]'), function(){
+      //  console.log("Connecting ...");
+      //});
+
+    });
+  }).run(function() {
+    test.done();
+  });
+
+
 });
 
 

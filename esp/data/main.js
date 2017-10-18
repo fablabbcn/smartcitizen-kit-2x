0f7e159c7fdb03985e371e5303f4685348389f5c
Viktor Smari <viktorsmari@gmail.com>
2017-10-18 18:32:55 +0200

    Notifications

--- esp/data/main.js
+++ esp/data/main.js
@@ -45,7 +45,7 @@ var app = new Vue({
 
     setTimeout(function() {
       return that.jsGet('aplist');
-    }, 500);
+    }, 1500);
 
     // This checks if connection to the kit has been lost, every 5 sec
     this.every5sec();
@@ -64,7 +64,6 @@ var app = new Vue({
       // the API should be on the same IP and port
       // Most likely a 192.168.*.1/status
 
-      this.errors.push('Select API url')
       // If we are using port 8000, we are in development, and the API should be on port 3000
       if (window.location.port === '8000') {
         this.theApi = 'http://' + window.location.hostname + ':3000/';
@@ -73,60 +72,54 @@ var app = new Vue({
         this.theApi = window.location.href;
       }
 
+      this.errors.push('Using API: ' + this.theApi);
       console.log('Using API : ' + this.theApi);
     },
     selectPath: function (path) {
       this.setuppath = path;
     },
-    jsGet: function (path, purpose) {
-      // Backup function to fetch with pure javascript
-      // (If we cannot use extra libraries due to space issues etc)
-      var that = this;
-
+    httpGet: function(theUrl) {
       var xmlHttp = new XMLHttpRequest();
-      xmlHttp.open("GET", this.theApi + path, false); // false for synchronous request
-      xmlHttp.send(null);
+      xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+      xmlHttp.send( null );
+      return xmlHttp.responseText;
+    },
+    jsGet: function(path) {
+      var that = this;
+      var res = this.httpGet(this.theApi + path);
 
-      if (path = 'online') {
-        that.wifis = JSON.parse(xmlHttp.responseText);
-        that.notification = 'JS Fetching wifi list';
+      if (path == 'aplist') {
+        that.wifis = JSON.parse(res);
+        that.notification = 'Getting wifi list..';
       }
-      if (path = 'status'){
-        that.notification = 'JS Fetching status';
-        that.browsertime = Math.floor(Date.now() / 1000);
-        //that.devicetime = response.data.time;
+      if (path == 'status'){
+        that.notification = 'Getting status..';
+        that.debuginfo = JSON.parse(res);
       }
 
     },
     // Not actually a POST request, but a GET with params
     jsPost: function(path, purpose){
       this.browsertime = Math.floor(Date.now() / 1000);
+      var res;
       var that = this;
       // Available parameters:
       // /set?ssid=value1&password=value2&token=value3&epoch=value
 
-      var xmlHttp = new XMLHttpRequest();
-
-      if (purpose == 'online'){
-        xmlHttp.open("GET", that.theApi + path +
+      if (purpose == 'connect'){
+        that.notification = 'Connecting online..';
+        res = that.httpGet(that.theApi + path +
             '?ssid=' + that.selectedWifi +
             '&password=' + that.wifipass +
             '&token=' + that.usertoken +
             '&epoch=' + that.browsertime);
-        xmlHttp.send(null);
-        console.log(xmlHttp.responseText);
-
-        console.log(response);
-        //  that.errors.push(e);
       }
 
       if (purpose == 'synctime'){
-        xmlHttp.open("GET", this.theApi + path + '?epoch=' + this.browsertime);
-        xmlHttp.send(null);
-        console.log('synctime GET response:');
-        // TODO: check if we get a 200 from device?
-        console.log(xmlHttp.responseText);
+        that.notification = 'Starting to log on SD CARD..';
+        res = this.httpGet(this.theApi +  path + '?epoch=' + this.browsertime);
       }
+      console.log(res);
 
     },
   },

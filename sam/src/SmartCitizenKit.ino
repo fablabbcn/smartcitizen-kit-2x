51583f7d039a0cf4cbe7498ed3299320ebc610ae
Victor Barberan <victor@smartcitizen.me>
2018-04-23 14:00:43 +0200

    missing interrupt handler on last commit

--- sam/src/SmartCitizenKit.ino
+++ sam/src/SmartCitizenKit.ino
@@ -28,6 +28,10 @@ void TC4_Handler (void) {
 	base.buttonStillDown();
 	TC4->COUNT16.INTFLAG.bit.MC0 = 1;
 }
+void TC3_Handler (void) {
+	base.timerAlarm();
+	TC3->COUNT16.INTFLAG.bit.MC0 = 1;
+}
 // void ISR_alarm() {
 // 	base.wakeUp();
 // };

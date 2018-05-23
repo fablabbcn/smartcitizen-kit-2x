6c98a93bef4a034d48e91a26f4acbcb150b76e18
Victor Barberan <victor@smartcitizen.me>
2018-05-23 10:56:08 +0200

    Temporal hardcoded calibration data Alphasense

--- sam/src/AlphaDelta.h
+++ sam/src/AlphaDelta.h
@@ -101,24 +101,24 @@ class AlphaDelta
 		AlphaSensor Slot1 = {
 			{ADC_1, MCP342X_CHANNEL_1, {0x55, 0x01}, initGain},		// Electrode A
 			{ADC_1, MCP342X_CHANNEL_2, {0x55, 0x00}, initGain},		// Electrode W
-			{ALPHA_CO, (-68.1), (-13.9), {601.9, 0}} // board U		// Gas type, zero current W, zero current A, sensitivity TODO move this to eeprom inside alpha board
-			/* {ALPHA_CO, (-69.4), (-18.6), {493.1, 0}} // board 1 */
+			/* {ALPHA_CO, (-68.1), (-13.9), {601.9, 0}} // board U		// Gas type, zero current W, zero current A, sensitivity TODO move this to eeprom inside alpha board */
+			{ALPHA_CO, (-69.4), (-18.6), {493.1, 0}} // board 1
 		};
 
 		// Slot 2 sensor
 		AlphaSensor Slot2 = {
 			{ADC_2_3, MCP342X_CHANNEL_3, {0x56, 0x01}, initGain},		// Electrode A
 			{ADC_2_3, MCP342X_CHANNEL_4, {0x56, 0x00}, initGain},		// Electrode W
-			{ALPHA_NO2, 31.5, 17.7, {(-383.7), 0}} // Board U
-			/* {ALPHA_NO2, 25.9, 15.4, {(-384.9), 0}} // Board 1 */
+			/* {ALPHA_NO2, 31.5, 17.7, {(-383.7), 0}} // Board U */
+			{ALPHA_NO2, 25.9, 15.4, {(-384.9), 0}} // Board 1
 		};
 
 		// Slot 3 sensor
 		AlphaSensor Slot3 = {
 			{ADC_2_3, MCP342X_CHANNEL_1, {0x54, 0x01}, initGain},		// Electrode A
 			{ADC_2_3, MCP342X_CHANNEL_2, {0x54, 0x00}, initGain},		// Electrode W
-			{ALPHA_NO2_O3, 23.01, 14.5, {(-446.36), (-506.96)}} // Board U
-			/* {ALPHA_NO2_O3, 23.33, 19.86, {(-466.29), (-466.29)}} // Board 1 */
+			/* {ALPHA_NO2_O3, 23.01, 14.5, {(-446.36), (-506.96)}} // Board U */
+			{ALPHA_NO2_O3, 23.33, 19.86, {(-466.29), (-466.29)}} // Board 1
 
 		};
 

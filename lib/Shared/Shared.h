5a3ff6d295cb104fbf723df60397eb0635faa62f
Victor Barberan <victor@smartcitizen.me>
2018-10-09 14:42:41 +0200

    Pushed SAM firmware version to 2.1

--- lib/Shared/Shared.h
+++ lib/Shared/Shared.h
@@ -50,7 +50,7 @@ enum SAMMessage {
 #define ESP_ADDRESS 2
 
 const String hardwareVer = "2.0";
-const String SAMversion	= "0.2.0";
+const String SAMversion	= "0.2.1";
 const String SAMbuildDate = String(__DATE__) + '-' + String(__TIME__);
 const String ESPversion = "0.2.0";
 const String ESPbuildDate = String(__DATE__) + '-' + String(__TIME__);

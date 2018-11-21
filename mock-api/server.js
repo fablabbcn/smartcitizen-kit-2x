6e0b7a35106210ecf4e703af9ef686857d584e46
Viktor Smari <viktorsmari@gmail.com>
2018-11-21 10:48:36 +0100

    Javascript: Use build_data

--- mock-api/server.js
+++ mock-api/server.js
@@ -3,7 +3,7 @@ var http = require('http')
 var serveStatic = require('serve-static')
 
 // Serve up public/ftp folder
-var serve = serveStatic('../esp/data', {'index': ['final.html']})
+var serve = serveStatic('../esp/build_data', {'index': ['index.html']})
 
 // Create server
 var server = http.createServer(function onRequest (req, res) {

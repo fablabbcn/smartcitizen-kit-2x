4c8284994883b60d599336dcd02e707256d16ae0
Victor Barberan <victor@smartcitizen.me>
2019-04-05 14:31:45 +0200

    Some sleep power optimizations

--- sam/src/SckBase.cpp
+++ sam/src/SckBase.cpp
@@ -78,7 +78,7 @@ void SckBase::setup()
 	sdDetect();
 
 	// Flash storage
-	/* readingsList.flashStart(); */
+	readingsList.flashStart();
 
 /* #define autoTest  // Uncomment for doing Gases autotest, you also need to uncomment  TODO complete this */
 
@@ -1267,13 +1267,17 @@ void SckBase::goToSleep()
 	digitalWrite(pinESP_RX_WIFI, LOW);
 	digitalWrite(pinESP_TX_WIFI, LOW);
 
-	// TODO check every component for power optimizations
-
 	// Stop PM sensor
 	if (urban.sck_pm.started) urban.sck_pm.stop();
 
+	// Turn off Auxiliary I2C bus
+	digitalWrite(pinPOWER_AUX_WIRE, HIGH);	// LOW -> ON , HIGH -> OFF
+
 	// Turn off USB led
 	digitalWrite(pinLED_USB, HIGH);
+
+	// Sd card savings
+	digitalWrite(pinCS_SDCARD, HIGH);
 
 	if (sckOFF) {
 

d0332fb307dd520bb38fa7124175d8b05b7bb991
Victor Barberan <victor@smartcitizen.me>
2020-12-02 18:59:37 +0100

    Changed MQTT and NTP to standard port numbers

--- lib/Shared/Config.h
+++ lib/Shared/Config.h
@@ -41,8 +41,8 @@ enum errorType {
 struct SensorConfig { bool enabled; uint8_t everyNint; bool oled_display=false; };
 struct Credentials { bool set=false; char ssid[64]="null"; char pass[64]="null"; };
 struct Token { bool set=false; char token[7]="null"; };
-struct Mqtt { char server[64]="mqtt.smartcitizen.me"; uint16_t port=80; };
-struct Ntp { char server[64]="ntp.smartcitizen.me"; uint16_t port=80; };
+struct Mqtt { char server[64]="mqtt.smartcitizen.me"; uint16_t port=1883; };
+struct Ntp { char server[64]="ntp.smartcitizen.me"; uint16_t port=123; };
 struct MAC { bool valid=false; char address[18]="not synced"; };
 struct BattConf { int16_t chargeCurrent=768; uint32_t battCapacity=2000; };
 struct Extra { bool ccsBaselineValid=false; uint16_t ccsBaseline; }; 			// Here we save variables that don't have an specific place

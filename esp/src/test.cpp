fcec176c66cd0e7b56523aaa126c93a84f571478
raztamim94@gmail.com <raztamim94@gmail.com>
2025-08-12 09:34:55 +0300

    test file to test the checks

--- esp/src/test.cpp
+++ esp/src/test.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     int ledPin = 13; // Magic number violation
-    std::cout << "LED pin: " << ledPin << std::endl;
+    std::cout << "LED pin: " << 13 << std::endl; // Magic number violation
     return 0;
 }

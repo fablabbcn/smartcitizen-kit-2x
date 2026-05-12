1556612b8fd6bd1d9692b6b5017e8c2152897584
oscgonfer <oscgonfer@gmail.com>
2026-05-12 13:59:56 +0200

    Add env variables for testing procedure

---
+++ sam/env.py
@@ -0,0 +1,20 @@
+from os.path import isfile
+Import("env")
+
+file_name = env.GetProjectOption("file_name")
+print (f"Checking {file_name} environment")
+
+assert isfile(file_name)
+try:
+  f = open(file_name, "r")
+  lines = f.readlines()
+  for line in lines:
+    l = line.strip().split("=")
+    print (f'Adding {l[0]}: {l[1]}')
+    env.Append(CPPDEFINES=[
+        (l[0], env.StringifyMacro(l[1])),
+    ])
+except IOError:
+  print(f"File {file_name} not accessible",)
+finally:
+  f.close()

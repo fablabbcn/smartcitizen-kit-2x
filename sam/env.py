05fe963835165c8de070f34e34d4f54a2585039d
oscgonfer <oscgonfer@gmail.com>
2026-05-12 15:49:39 +0200

    Add defaults to test credentials on env.py

--- sam/env.py
+++ sam/env.py
@@ -4,17 +4,26 @@ Import("env")
 file_name = env.GetProjectOption("file_name")
 print (f"Checking {file_name} environment")
 
-assert isfile(file_name)
-try:
-  f = open(file_name, "r")
-  lines = f.readlines()
-  for line in lines:
-    l = line.strip().split("=")
-    print (f'Adding {l[0]}: {l[1]}')
-    env.Append(CPPDEFINES=[
-        (l[0], env.StringifyMacro(l[1])),
-    ])
-except IOError:
-  print(f"File {file_name} not accessible",)
-finally:
-  f.close()
+if not isfile(file_name):
+  print ("File doesnt exist")
+  l={
+    "SC_TEST_WIFI_SSID":"fake",
+    "SC_TEST_WIFI_PASSWD":"fake"
+  }
+  env.Append(CPPDEFINES=[
+    (l[0], env.StringifyMacro(l[1])),
+  ])
+else:
+  try:
+    f = open(file_name, "r")
+    lines = f.readlines()
+    for line in lines:
+      l = line.strip().split("=")
+      print (f'Adding {l[0]}: {l[1]}')
+      env.Append(CPPDEFINES=[
+          (l[0], env.StringifyMacro(l[1])),
+      ])
+  except IOError:
+    print(f"File {file_name} not accessible",)
+  finally:
+    f.close()

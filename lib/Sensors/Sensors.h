e8859ad73353998a0acb34fd0ee4781247c22a67
oscgonfer <oscgonfer@gmail.com>
2026-08-23 12:40:27 +0200

    Add IDs for SCD4X

--- lib/Sensors/Sensors.h
+++ lib/Sensors/Sensors.h
@@ -596,14 +596,13 @@ class AllSensors
             OneSensor { BOARD_AUX,      100,    SENSOR_AS7341_FLICKER_P2P,      "AS7341_FLICKER_P2P", "AS7341 Flicker Peak to Peak",        256,    true,       1,  "",         false         },
 #endif
 
-#ifdef SCK_WITH_SENSOR_GROVE_OLED
 #ifdef SCK_WITH_SCD4X
-            OneSensor { BOARD_AUX,      100,    SENSOR_SCD4X_CO2,               "SCD4X_CO2",        "SCD4X CO2",                            0,    true,       1,  "ppm"                      },
-            OneSensor { BOARD_AUX,      100,    SENSOR_SCD4X_TEMP,              "SCD4X_TEMP",       "SCD4X Temperature",                    0,    true,       1,  "C",         false         },
-            OneSensor { BOARD_AUX,      100,    SENSOR_SCD4X_HUM,               "SCD4X_HUM",        "SCD4X Humidity",                       0,    true,       1,  "%" ,        false         },
+            OneSensor { BOARD_AUX,      100,    SENSOR_SCD4X_CO2,               "SCD4X_CO2",        "SCD4X CO2",                            258,    true,       1,  "ppm"                      },
+            OneSensor { BOARD_AUX,      100,    SENSOR_SCD4X_TEMP,              "SCD4X_TEMP",       "SCD4X Temperature",                    259,    true,       1,  "C",         false         },
+            OneSensor { BOARD_AUX,      100,    SENSOR_SCD4X_HUM,               "SCD4X_HUM",        "SCD4X Humidity",                       260,    true,       1,  "%" ,        false         },
 #endif
 
-#ifdef WITH_SENSOR_GROVE_OLED
+#ifdef SCK_WITH_SENSOR_GROVE_OLED
             // Later this will be moved to a Actuators.h file
             // Groove I2C Oled Display 96x96
             OneSensor { BOARD_AUX,      250,    SENSOR_GROVE_OLED,              "GR_OLED",          "Groove OLED",                          0,      true,       1,  "",          false         },
